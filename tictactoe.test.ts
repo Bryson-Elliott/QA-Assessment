109ee1353a0494454a8f3cbc88b1ed01807106ff
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-09-24 11:59:10 -0600

    Finished with most stuff

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -37,3 +37,24 @@ test('Can X be put in the bottom right?', async () => {
     let x3 = await (await driver).findElement(By.id('cell-8'))
     await x3.click()
 })
+
+test('Check for winner', async () => {
+    
+    let check = await (await driver).findElement(By.id('h1', === " lost"))
+    await check.click()
+})
+// Changed it to won so that it would look better when deployed^
+
+test('Check O', async () => {
+
+    let O = await (await driver).findElement(By.id('cell-3', "o"))
+    await O.click()
+})
+// Changed it to uppercase "O" for uniformity
+
+test('Look for covers!', async () => {
+
+    let xo = await (await driver).findElement(By.(''))
+    await xo.click()
+})
+// Little stuck here not sure how to check for it
